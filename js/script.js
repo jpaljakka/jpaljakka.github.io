bfb976561c6f0f25ec32cb6b74530f875e895e02
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-02-07 12:14:14 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //navigation display functions//
 
-$(document).ready(function () {
+$(document).ready(function() {
   $("#navHome").hide(0, onloadProperties);
   $("#navAbout").hide(0, onloadProperties);
   $("#navContact").hide(0, onloadProperties);
@@ -9,33 +9,49 @@ $(document).ready(function () {
 });
 
 function onloadProperties() {
-  $(document).ready(function () {
+  $(document).ready(function() {
     $("#navHome").fadeIn(2000);
     $("#navAbout").fadeIn(3000);
     $("#navContact").fadeIn(4000);
     $("#logo").fadeTo(5000, 1);
-
   });
 }
 
 //Back to top button//
 
-$(window).scroll(function () {
+$(window).scroll(function() {
   var height = $(window).scrollTop();
   if (height > 150) {
-    $('#myBtn').fadeIn();
+    $("#myBtn").fadeIn();
   } else {
-    $('#myBtn').fadeOut();
+    $("#myBtn").fadeOut();
   }
 });
 
-$(document).ready(function () {
-  $("#myBtn").click(function (event) {
+$(document).ready(function() {
+  $("#myBtn").click(function(event) {
     event.preventDefault();
-    $("html, body").animate({
-      scrollTop: 0
-    }, "fast");
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      "fast"
+    );
     return false;
   });
-
 });
+jQuery(document).ready(function() {
+  delay();
+});
+
+function delay() {
+  var secs = 3000;
+  setTimeout("initFadeIn()", secs);
+}
+
+function initFadeIn() {
+  jQuery(".hide").css("visibility", "visible");
+  jQuery("body").css("display", "none");
+  jQuery("body").fadeIn(1200);
+  jQuery(".sk-folding-cube").css("visibility", "hidden");
+}
